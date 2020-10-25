f64684669fa8e5467ca7212de2316600a787d1c9
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-10-25 11:42:08 +0200

    Fighting private visibility
    makes me use singletons. Simplified the initializtion process.

--- deep/lexer.cpp
+++ deep/lexer.cpp
@@ -1,64 +1,24 @@
- #include "swift/Frontend/Frontend.h"
+#include "swift/Basic/SourceManager.h"
+#include "swift/Basic/LangOptions.h"
+#include "swift/AST/ASTContext.h"
+#include "swift/AST/DiagnosticEngine.h"
 
-// Use Swift Lexer
+/// Use Swift Lexer
+
+/*
+                    swift::SourceManager
+                                  \/
+  swift::ast::ASTContext - needs SourceManager, Opt*-like classes,
+               and DiagnosticEngine
+
+ */
 
 int main() {
-
-
-
-
-
-
-
-
-
-
-
-
-  // just get a swift::CompilerInstance and call its method
-  // CompilerInstance::setUpASTContextIfNeeded(), but in simplified
-  // form - you will have to remove some of it
-  // This is the highest level I should start working from.
-
-  // SetUpASTContextIfNeede
-  // CompilerInstance::setUpASTContextIfNeeded()
-
-  // needs swift::SourceManager
-
-  // swift::ClangImporter::create() creates everything!
-  // (needs swift::ASTContext)
-  //
-  /*
-      *** there is some stuff before that
-      // Set up the file manager.
-      {
-      llvm::IntrusiveRefCntPtr<llvm::vfs::FileSystem> VFS =
-      clang::createVFSFromCompilerInvocation(instance.getInvocation(),
-      instance.getDiagnostics(),
-      ctx.SourceMgr.getFileSystem(
-
-      ));
-      instance.createFileManager(std::move(VFS));
-
-      }
-   */
-
-  // ClangImporter::create() creates everything!
-  // clang::CompielerInstance::createFileManager(llvm::vfs::FileSystem)
-  // llvm::IntrusiveRefCntPtr<llvm::vfs::FileSystem>
-  // FileManager
-  // SourceManager clang::namespace  | clang namespace
-  // ---------------------------------
-  //                                 | it is a all swift:: namespace
-  //
-  // ... easier to creaet ASTContext |
-  //                                  for some reason,
-  //                                  clang::ASTContext is also
-  //                                  constructed by ClangImporter.
-  //
-  // SwiftIdentifier | ASTContext
-  // ModuleDecl
-  // SourceFile
-  // Lexer
+  swift::SourceManager mgr;
+  swift::DiagnosticEngine eng(mgr);
+  swift::LangOptions lang_opts;
+  swift::TypeCheckerOptions tch_opts;
+  swift::SearchPathOptions sp_opts;
+  swift::ASTContext::get(lang_opts, tch_opts, sp_opts, mgr, eng);
   return 0;
 }
