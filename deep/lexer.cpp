534474764682acbd2ef2236e2987d089cb2d8841
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-10-25 15:12:10 +0200

    Outlined the next steps.

--- deep/lexer.cpp
+++ deep/lexer.cpp
@@ -31,12 +31,21 @@ int main() {
 
   // Nothing is primary here
   swift::InputFile in("main.swift", false);
-  std::cout << in.file();
-
+  std::cout << in.file() << std::endl;
+  std::cout << in.buffer() << std::endl;
+  // clangimporter::create
   swift::DiagnosticEngine eng(mgr);
   swift::LangOptions lang_opts;
   swift::TypeCheckerOptions tch_opts;
   swift::SearchPathOptions sp_opts;
   swift::ASTContext::get(lang_opts, tch_opts, sp_opts, mgr, eng);
+
+  // now that we have a valid context, we can do what
+  // ClangImporter::create() does.
+
+  // SourceManager, BufferID
+  // 1 - get bufferid from source manager inside astcontext???
+  // 2 - how tf we initialize source files?????? memory buffer is still
+  // empty!!!
   return 0;
 }
