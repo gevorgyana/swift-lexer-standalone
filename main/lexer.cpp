b363a2186a894127ab487276b6e57594fd3c7206
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-10-26 19:48:16 +0200

    A small rule to use for debugging.

--- main/lexer.cpp
+++ main/lexer.cpp
@@ -1,9 +1,18 @@
 #include "Lexer.h"
 #include "swift/Basic/LangOptions.h"
 
+#include <fstream>
+#include <string>
+#include <iostream>
+
 int main() {
   swift::LangOptions langOpts;
-  std::string contents("hello world");
+  std::ifstream ifs("test/main.swift");
+  std::string contents(
+      (std::istreambuf_iterator<char>(ifs)),
+      (std::istreambuf_iterator<char>())
+  );
+  std::cout << contents << std::endl;
   swift::LexerMode lexMode;
   swift::Lexer L(
       langOpts,
