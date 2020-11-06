81d0484bf1ca1e084f3bb6a16f60e04522654b2a
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-11-06 23:05:46 +0200

    Prepare a new shim using macro metaprogramming.

--- main/generate_clang_tokens.cpp
+++ main/generate_clang_tokens.cpp
@@ -1,10 +1,22 @@
-// #include "clang/Basic/TokenKinds.h"
-		      
-#define OBJC_AT_KEYWORD(X) objc_##X,
-		      #include "clang/Basic/TokenKinds.def"
-		        NUM_OBJC_KEYWORDS
-};
+// Violent X macros usage, inspired by the Swift and Clang teams.
+// This is metaprogramming anyway ;)
+
+// As they are using StringRef in their code, and I don't want to break ODR,
+// I should just make sure the compiler knows what that is. This is a workaround, though.
+#include "llvm/ADT/StringRef.h"
+using namespace llvm;
+
+// this defines the Swift token enum
+#include "swift/Syntax/TokenKinds.h"
+#include "clang/Basic/TokenKinds.h"
+
+#define TOKEN(X) \
+  case swift::tok::X: return clang::tok::TokenKind::X;
 
 int main() {
-  return 0;
+  auto foo = swift::tok::eof;
+  switch(foo) {
+    #include "swift/Syntax/TokenKinds.def"
+    NUM_TOKENS
+  }
 }
