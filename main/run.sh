899406596d911d0cc6a94df81a247dbe9799e28a
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-12-08 00:07:54 +0200

    Add, for convenience, my wrapper around Makefile

---
+++ main/run.sh
@@ -0,0 +1,4 @@
+# Do not forget to set command-line arguments
+SWIFT_CONFIGURED_SOURCES=/home/artyom/proj/formatter/swift SWIFT_BINARIES=/home/artyom/proj/swift/swift-5.3-RELEASE-ubuntu20.04/usr/lib make all
+
+# /home/artyom/proj/swift/swift-5.3-RELEASE-ubuntu20.04/usr/lib
